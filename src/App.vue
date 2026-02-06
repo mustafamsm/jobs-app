2b47fd9d018a7abebca15b6422a01e416053faeb
mustafamsm <75886186+mustafamsm@users.noreply.github.com>
2026-02-06 22:17:25 +0200

    setupt project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-center text-red-600   ">Weather App</h1>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+ 
+</script>
+
+<style lang="scss" scoped>
+
+</style>
